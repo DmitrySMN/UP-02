dee25d6f8a4a86eeb55377894468baf7eb9b710b
student251 <ddmtry064@gmail.com>
2024-12-03 11:37:05 +0300

    CLOSE #1. Реализована возможность импорта csv файлов

--- DemoEx/AdminForm.Designer.cs
+++ DemoEx/AdminForm.Designer.cs
@@ -30,18 +30,18 @@ namespace DemoEx
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tables = new System.Windows.Forms.ComboBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.tables);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(347, 100);
@@ -49,13 +49,13 @@ namespace DemoEx
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Таблица для импорта";
             // 
-            // comboBox1
+            // tables
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(6, 38);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(335, 40);
-            this.comboBox1.TabIndex = 0;
+            this.tables.FormattingEnabled = true;
+            this.tables.Location = new System.Drawing.Point(6, 38);
+            this.tables.Name = "tables";
+            this.tables.Size = new System.Drawing.Size(335, 40);
+            this.tables.TabIndex = 0;
             // 
             // openFileDialog1
             // 
@@ -72,6 +72,7 @@ namespace DemoEx
             this.button1.TabIndex = 1;
             this.button1.Text = "Выбрать файл для импорта";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -82,20 +83,8 @@ namespace DemoEx
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(347, 46);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Восстановить базу данныхВосстановить базу данных";
+            this.button2.Text = "Восстановить БД";
             this.button2.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.Black;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(12, 170);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(347, 46);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Импорт";
-            this.button3.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
@@ -106,6 +95,16 @@ namespace DemoEx
             this.button4.TabIndex = 5;
             this.button4.Text = "Выйти";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "или";
             // 
             // AdminForm
             // 
@@ -114,8 +113,8 @@ namespace DemoEx
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(371, 339);
             this.ControlBox = false;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
@@ -123,21 +122,23 @@ namespace DemoEx
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
             this.Name = "AdminForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Импорт данных";
             this.Load += new System.EventHandler(this.AdminForm_Load);
             this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox tables;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
     }
 }
--- DemoEx/AdminForm.cs
+++ DemoEx/AdminForm.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace DemoEx
 {
@@ -19,7 +21,88 @@ namespace DemoEx
 
         private void AdminForm_Load(object sender, EventArgs e)
         {
+            openFileDialog1.Filter = "CSV files (*.csv)|*.csv|XML files (*.xml)|*.xml";
 
+            tables.DropDownStyle = ComboBoxStyle.DropDownList;
+            tables.Items.Add("Клиенты");
+            tables.Items.Add("Объекты");
+            tables.Items.Add("Сотрудники");
+            tables.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "c:\\";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string pathToFile = openFileDialog1.FileName; // full path
+                importCsvToDatabase(pathToFile);
+            }
+            else
+            {
+                MessageBox.Show("Выберите файл!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void importCsvToDatabase(string filePath)
+        {
+            string con = Connection.getConnectionString();
+            using (MySqlConnection connection = new MySqlConnection(con))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        using (var reader = new StreamReader(filePath))
+                        {
+                            string headerLine = reader.ReadLine();
+                            string[] headers = headerLine.Split(';');
+                            while (!reader.EndOfStream)
+                            {
+                                string line = reader.ReadLine();
+                                string[] values = line.Split(';');
+                                
+                                switch (tables.Text)
+                                {
+                                    case "Клиенты":
+                                        string sql = "INSERT INTO `clients` (" + string.Join(",", headers) + ") VALUES (" + string.Join(",", values.Select(v => "'" + MySqlHelper.EscapeString(v) + "'")) + ")";
+                                        using (MySqlCommand command = new MySqlCommand(sql, connection, transaction))
+                                        {
+                                            command.ExecuteNonQuery();
+                                        }
+                                        break;
+                                    case "Объекты":
+                                        string sql1 = "INSERT INTO `estate` (" + string.Join(",", headers) + ") VALUES (" + string.Join(",", values.Select(v => "'" + MySqlHelper.EscapeString(v) + "'")) + ")";
+                                        using (MySqlCommand command = new MySqlCommand(sql1, connection, transaction))
+                                        {
+                                            command.ExecuteNonQuery();
+                                        }
+                                        break;
+                                    case "Сотрудники":
+                                        string sql2 = "INSERT INTO `employees` (" + string.Join(",", headers) + ") VALUES (" + string.Join(",", values.Select(v => "'" + MySqlHelper.EscapeString(v) + "'")) + ")";
+                                        using (MySqlCommand command = new MySqlCommand(sql2, connection, transaction))
+                                        {
+                                            command.ExecuteNonQuery();
+                                        }
+                                        break;
+                                }
+                            }
+                        }
+                        transaction.Commit();
+                        MessageBox.Show("Данные успешно импортированы.");
+                    } catch (Exception excc)
+                    {
+                        transaction.Rollback();
+                        MessageBox.Show("Ошибка импорта данных: " + excc.Message);
+                    }
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
